981ac2647caca12ea502abb94ab712674c2ce37b
smatovalmaz <almsm260298@gmail.com>
2024-06-19 01:59:36 +0600

    task.py

--- task.py
+++ task.py
@@ -1,3 +1,18 @@
-from art import tprint
-tprint("PYTHON")
+from class1 import NewHello
+
+instance = NewHello("Alice")
+
+result = instance.tptint()
+print(result)
+
+
+# class NewHello():
+#     def __init__(self, name):
+#         super().__init__(name)
+#
+#     def tptint(self):
+#         return f"TPTINT applied to {self.name}"
+
+
+
 
